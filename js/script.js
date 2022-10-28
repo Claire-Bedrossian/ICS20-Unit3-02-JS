f283604ac0c8a473b4610801ab24ad6594b7e293
Claire-Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-28 14:31:41 -0400

    added functions

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-Unit3-02-JS/sw.js", {
+    scope: "/ICS20-Unit3-02-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function calculates the volume of a right rectangular based pyramid with user input
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  // input
+  const length = parseInt(document.getElementById("length-number").value)
+  const width = parseInt(document.getElementById("width-number").value)
+  const height = parseInt(document.getElementById("height-number").value)
+
+  //process
+  const volume = (length * width * height) / 3 * height
+
+  // output
+  document.getElementById("volume").innerHTML =
+    "Volume is equal to: " + volume + " cm³"
 }
